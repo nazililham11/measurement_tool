7c1841353f0c7798f2376acf58b6201b79178075
nazililham11 <nazil.ilham11@gmail.com>
2020-11-12 11:20:12 +0700

    Add SoftAP Mode

--- Arduino/VehicleMeasuring/VehicleMeasuring.ino
+++ Arduino/VehicleMeasuring/VehicleMeasuring.ino
@@ -2,15 +2,24 @@
 #include <ESP8266WiFi.h>
 #include <NewPing.h>
 
+#define SOFT_AP_MODE
+
 using namespace websockets;
+
+const char* AP_WIFI_SSID        = "SoftAP";      //Access Point SSID
+const char* AP_WIFI_PASSWORD    = "12345678";    //Access Point Password
+
+const IPAddress ap_ipAddress(10, 10, 10, 1);
+const IPAddress ap_gateway(10, 10, 10, 1);
+const IPAddress ap_subnetMask(255, 255, 255, 0);                              
 
 const uint8_t TRIGGER_PIN       = 4;
 const uint8_t ECHO_PIN          = 5;
 const uint8_t MAX_DISTANCE      = 500;              //Cm
 const uint8_t SCAN_TIMES        = 60;
 
-const char* WIFI_SSID           = "TP-LINK";        //Enter SSID
-const char* WIFI_PASSWORD       = "wifinerusak";    //Enter Password
+const char* WIFI_SSID           = "SoftAP";        //Enter SSID
+const char* WIFI_PASSWORD       = "12345678";      //Enter Password
 
 const uint8_t WEBSOCKET_PORT    = 80;
 
@@ -23,7 +32,11 @@ NewPing Sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 void setup() {
 	Serial.begin(115200);
 	
-	wifi_connect();
+    #ifdef SOFT_AP_MODE 
+        wifi_access_point_start();
+    #else
+	    wifi_connect();
+    #endif
     websocket_init();
 }
 
@@ -101,4 +114,18 @@ unsigned int sonar_read() {
     Serial.println("cm");
 
     return distance;
+}
+
+void wifi_access_point_start() {
+    Serial.print("Starting SoftAP");
+    
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.softAPConfig(ap_ipAddress, ap_gateway, ap_subnetMask);   // subnet FF FF FF 00
+    WiFi.softAP(AP_WIFI_SSID, AP_WIFI_PASSWORD);
+    
+    IPAddress myIP = WiFi.softAPIP();
+    
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+    Serial.println("Done");
 }
