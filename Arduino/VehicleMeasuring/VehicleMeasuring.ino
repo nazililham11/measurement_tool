36738f96440d494c4bce7977bc4046d0f9a79070
nazililham11 <nazil.ilham11@gmail.com>
2020-12-21 19:25:30 +0700

    - Penambahan LCD - Perubahan Skema - Perubahan Library WebSocket - Penambahan Flowchart

--- Arduino/VehicleMeasuring/VehicleMeasuring.ino
+++ Arduino/VehicleMeasuring/VehicleMeasuring.ino
@@ -1,89 +1,139 @@
-#include <ArduinoWebsockets.h>
 #include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <NewPing.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
-//#define SOFT_AP_MODE
+const char* AP_WIFI_SSID            = "SoftAP";             //Access Point SSID
+const char* AP_WIFI_PASSWORD        = "12345678";           //Access Point Password
 
-using namespace websockets;
+const char* WIFI_SSID               = "MyWifi";             //Wifi SSID
+const char* WIFI_PASSWORD           = "12345678";           //Wifi Password
 
-const char* AP_WIFI_SSID        = "SoftAP";      //Access Point SSID
-const char* AP_WIFI_PASSWORD    = "12345678";    //Access Point Password
+const IPAddress ap_ipAddress        (10, 10, 10, 1);        //Server IP Address
+const IPAddress ap_gateway          (10, 10, 10, 1);        //Server Gateway
+const IPAddress ap_subnetMask       (255, 255, 255, 0);     //Server Subnetmask                                   
 
-const IPAddress ap_ipAddress(10, 10, 10, 1);
-const IPAddress ap_gateway(10, 10, 10, 1);
-const IPAddress ap_subnetMask(255, 255, 255, 0);                              
+const uint8_t TRIGGER_PIN           = 12;
+const uint8_t ECHO_PIN              = 14;
+const uint8_t MAX_DISTANCE          = 600;                  //Cm
+const uint8_t SCAN_TIMES            = 10;
 
-const uint8_t TRIGGER_PIN       = 4;
-const uint8_t ECHO_PIN          = 5;
-const uint8_t MAX_DISTANCE      = 500;              //Cm
-const uint8_t SCAN_TIMES        = 60;
+const uint8_t WEBSOCKET_PORT        = 80;
 
-const char* WIFI_SSID           = "TP-LINK";        //Enter SSID
-const char* WIFI_PASSWORD       = "wifinerusak";      //Enter Password
+const uint8_t LCD_ADDRESS           = 0x27;
+const uint8_t LCD_COLS              = 16;
+const uint8_t LCD_ROWS              = 2;
 
-const uint8_t WEBSOCKET_PORT    = 80;
+const int32_t DEBUG_BAUD_RATE       = 115200;
+const uint8_t WIFI_CONNECT_TIMEOUT  = 15;                   //Detik
 
-WebsocketsServer Server;
+unsigned int distance; 
+bool access_point_mode;
 
-
-// NewPing setup of pins and maximum distance.
 NewPing Sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLS, LCD_ROWS);  
 
-void setup() {
-	Serial.begin(115200);
-	
-    wifi_access_point_start();
-    websocket_init();
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/");
+
+void wifi_init();
+void wifi_softap_start();
+void lcd_init();
+void lcd_show_distance();
+void websocket_send_distance();
+void websocket_handle_message(void *arg, uint8_t *data, size_t len);
+void websocket_onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
+void websocket_init();
+unsigned int sonar_read();
+    
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(DEBUG_BAUD_RATE);
+  
+  lcd_init();
+  wifi_init();
+  websocket_init();
+
+  // Start server
+  server.begin();
 }
 
 void loop() {
-    while (Server.available()){
-        
-        WebsocketsClient client = Server.accept();
-        client.onEvent(onEventsCallback);
+    distance = sonar_read();
+    lcd_update();
 
-        // run callback when messages are received
-        client.onMessage([&](WebsocketsMessage message) {
-        
-            String data = String(message.data());
-            Serial.print("Got Message: ");
-            Serial.println(message.data());
-        
-            if (data.equals("ping")){
-                unsigned int distance = sonar_read();
-                client.send("Distance: " + String(distance));
-                Serial.print("Send Message: ");
-                Serial.println(distance); 
-            }
-            
-        });
+    Serial.println("Distance :"+String(distance));
+    ws.cleanupClients();
 
-        while(client.available()){
-            client.poll();
-        }
-
-    }
-    delay(1000);
+    delay(100);
 }
 
+void wifi_init() {
+    Serial.println("Try connect to WiFi \"" + String(WIFI_SSID) + "\"");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-void onEventsCallback(WebsocketsEvent event, String data) {
-    if(event == WebsocketsEvent::ConnectionOpened) {
-        Serial.println("Connnection Opened");
-    } else if(event == WebsocketsEvent::ConnectionClosed) {
-        Serial.println("Connnection Closed");
-    } else if(event == WebsocketsEvent::GotPing) {
-        Serial.println("Got a Ping!");
-    } else if(event == WebsocketsEvent::GotPong) {
-        Serial.println("Got a Pong!");
+    for (int i = 0; i < 15 && WiFi.status() != WL_CONNECTED; i++) {
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.println(""); 
+    if (WiFi.status() != WL_CONNECTED){
+        Serial.println("Cannot connect to Wifi");
+        wifi_softap_start();
+    } else if (WiFi.status() == WL_CONNECTED) {
+        Serial.println("WiFi connected");
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
+        access_point_mode = false;
     }
 }
 
+void wifi_softap_start() {
+    Serial.print("Starting SoftAP");
+    
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(ap_ipAddress, ap_gateway, ap_subnetMask);   // subnet FF FF FF 00
+    WiFi.softAP(AP_WIFI_SSID, AP_WIFI_PASSWORD);
+    
+    Serial.print("AP IP address: ");
+    Serial.println(WiFi.softAPIP());
+    Serial.println("Done");
 
-void websocket_init(){
-	Server.listen(WEBSOCKET_PORT);
-	Serial.print("Is Server live? ");
-	Serial.println(Server.available());
+    access_point_mode = true;
+}
+
+void lcd_init(){
+    lcd.init();
+    lcd.backlight();
+    lcd.clear();
+}
+
+void lcd_update(){
+    lcd.clear();
+    lcd_set_distance();
+    lcd_set_wifi();
+}
+
+void lcd_set_distance() {
+    String text = "Jarak : " + String(distance) + " cm";
+    lcd.setCursor(0, 0);
+    lcd.print(text);
+}
+
+void lcd_set_wifi() {
+    String text = "";
+    lcd.setCursor(0, 1);
+    if (access_point_mode){
+        text = "AP |" + WiFi.softAPIP().toString();
+    } else {
+        text = "STA|" + WiFi.softAPIP().toString();
+    }
+    lcd.print(text);
 }
 
 unsigned int sonar_read() {
@@ -97,16 +147,67 @@ unsigned int sonar_read() {
     return distance;
 }
 
-void wifi_access_point_start() {
-    Serial.print("Starting SoftAP");
-    
-    WiFi.mode(WIFI_AP_STA);
-    WiFi.softAPConfig(ap_ipAddress, ap_gateway, ap_subnetMask);   // subnet FF FF FF 00
-    WiFi.softAP(AP_WIFI_SSID, AP_WIFI_PASSWORD);
-    
-    IPAddress myIP = WiFi.softAPIP();
-    
-    Serial.print("AP IP address: ");
-    Serial.println(myIP);
-    Serial.println("Done");
+void websocket_send_distance_old() {
+    String response = "Distance: " + String(distance);
+    ws.textAll(response);
+}
+
+void websocket_send_distance() {
+    String response = "{\"dst\":" + String(distance) + "}";
+    ws.textAll(response);
+}
+
+void websocket_send_status() {
+    String response = "{";
+    if (access_point_mode){
+        response += "\"mode\":\"AP\",";
+        response += "\"ssid\":\"" + String(AP_WIFI_SSID) + "\",";
+        response += "\"pass\":\"" + String(AP_WIFI_PASSWORD) + "\",";
+        response += "\"ip\":\"" + WiFi.softAPIP().toString() + "\",";
+    } else {
+        response += "\"mode\":\"STA\",";
+        response += "\"ssid\":\"" + String(WIFI_SSID) + "\",";
+        response += "\"pass\":\"" + String(WIFI_PASSWORD) + "\",";
+        response += "\"ip\":\"" + WiFi.localIP().toString() + "\",";
+    }
+    response += "\"uptime\":" + String(millis());   
+    response += "}";       
+    ws.textAll(response);
+}
+
+void websocket_handle_message(void *arg, uint8_t *data, size_t len) {
+    AwsFrameInfo *info = (AwsFrameInfo*)arg;
+    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+        data[len] = 0;
+        if (strcmp((char*)data, "ping") == 0) {
+            websocket_send_distance_old();
+        } else if (strcmp((char*)data, "scan") == 0) {
+            websocket_send_distance();
+        } else if (strcmp((char*)data, "status") == 0) {
+            websocket_send_status();
+        }
+    }
+}
+
+void websocket_onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, 
+    AwsEventType type, void *arg, uint8_t *data, size_t len) {
+    if (type == WS_EVT_CONNECT) {
+        Serial.println("Connnection Opened");
+        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+    } else if (type == WS_EVT_DISCONNECT) {
+        Serial.println("Connnection Closed");
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+    } else if (type == WS_EVT_DATA) {
+        Serial.println("Got a Data!");
+        websocket_handle_message(arg, data, len);
+    } else if (type == WS_EVT_PONG) {
+        Serial.println("Got a Pong!");
+    } else if (type == WS_EVT_ERROR) {
+        Serial.println("Got a Error!");
+    }
+}
+
+void websocket_init() {
+    ws.onEvent(websocket_onEvent);
+    server.addHandler(&ws);
 }
--- Readme.md
+++ Readme.md
@@ -1,14 +1,65 @@
-# Vehicle Measuring
+# Measurement Tool
+## Android
 
+***Konfigurasi Alamat Hardware***
 
-## Komponen
+Alamat Hardware dapat dilihat dari IP Address yang muncul pada LCD hardware 
+Misal IP Address Pada LCD adalah `192.168.0.100` maka alamat nya `ws://192.168.0.100`
+
+## Arduino
+
+### Komponen
  - NodeMCU ESP8266
  - Sensor Ultrasonic
- - Batterai 18650
- - Modul Charger 
- - Dc to Dc Stepup 5v
+ - Batterai
  - Saklar
+ - LCD 16x2
+ - LCD I2C Backpack/LCD to I2C Adapter
+
+### Skema
+![Skema v2](Assets/skema.png)
+
+### Flowchart Program Arduino
+![Flowchart Program Arduino v1](Assets/flowchart-arduino.png)
 
 
-## Skema
-![Skema v1.1](Assets/Skema.png)
+
+#### Konfigurasi WiFi
+Terdapat dua mode pada wifi yaitu `Station` dan `Access Pont`
+
+- Station 		: Terhubung ke wifi 
+- Access Point 	: Membuat wifi hotspot
+
+Saat dinyalakan Arduino akan mencoba untkuk terhubung ke WiFi yang ditentukan apabila setelah 15 detik arduino tidak terhubung ke WiFi maka arduino akan membuat Hotspot/Access Point sendiri 
+
+
+***Konfigurasi Wifi pada mode Access Point***
+```
+const char* AP_WIFI_SSID            = "SoftAP";             //Access Point SSID
+const char* AP_WIFI_PASSWORD        = "12345678";           //Access Point Password
+```
+
+***Konfigurasi Wifi pada mode Station***
+```
+const char* WIFI_SSID               = "MyWifi";           	//Wifi SSID
+const char* WIFI_PASSWORD           = "12345678";           //Wifi Password
+```
+
+
+#### Konfigurasi Sensor
+
+Proses pengukauran dilakukan sebanyak 10 kali untuk mendapatkan nilai yang akurat. Semakin banyak proses pengukuran maka akan semakin akurat tetapi akan memakan waktu lebih lama
+
+***Konfigurasi banyaknya proses pengukuran***
+```
+const uint8_t SCAN_TIMES            = 10;
+```
+
+
+#### Konfigurasi LCD
+***Konfigurasi Alamat LCD bisa disesuaikan dengan LCD yang digunakan***
+
+Untuk mengetahui alamat lcd bisa menggunakan program `I2C_Address_Scaner`
+```
+const uint8_t LCD_ADDRESS           = 0x27;
+```
